bd479d6f2c7078ed4646f41590b898d728c5e061
anst9000 <coltlaboy@yahoo.se>
2020-09-22 19:29:51 +0200

    Snipcart and Formspree setup

---
+++ src/components/Home/Menu.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react"
+import Title from "../Globals/Title"
+import Img from "gatsby-image"
+
+const getCategories = items => {
+  let tempItems = items.map(items => {
+    return items.node.category
+  })
+  let tempCategories = new Set(tempItems)
+  let categories = Array.from(tempCategories)
+
+  categories = ["all", ...categories]
+  return categories
+}
+
+export default class Menu extends Component {
+  constructor(props) {
+    super()
+    this.state = {
+      items: props.items.edges,
+      coffeItems: props.items.edges,
+      categories: getCategories(props.items.edges),
+    }
+  }
+
+  handleItems = category => {
+    let tempItems = [...this.state.items]
+    if (category === "all") {
+      this.setState(() => {
+        return { coffeItems: tempItems }
+      })
+    } else {
+      let items = tempItems.filter(({ node }) => node.category === category)
+      this.setState(() => {
+        return { coffeItems: items }
+      })
+    }
+  }
+
+  render() {
+    if (this.state.items.length > 0) {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="best of our menu" />
+            {/* Categories */}
+            <div className="row mb-5">
+              <div className="col-10 mx-auto text-center">
+                {this.state.categories.map((category, index) => {
+                  return (
+                    <button
+                      type="button"
+                      key={index}
+                      className="btn btn-yellow text-capitalize m-3"
+                      onClick={() => {
+                        this.handleItems(category)
+                      }}
+                    >
+                      {category}
+                    </button>
+                  )
+                })}
+              </div>
+            </div>
+            {/* Items */}
+            <div className="row">
+              {this.state.coffeItems.map(({ node }) => {
+                return (
+                  <div
+                    key={node.id}
+                    className="row col-12 col-lg-6 my-3 mx-auto"
+                  >
+                    <div className="col-1">
+                      <Img fixed={node.image.fixed} />
+                    </div>
+
+                    <div className="col-11">
+                      {/* Item content */}
+                      <div className="flex-grow-1 px-3">
+                        <div className="d-flex justify-content-between">
+                          <h6 className="mb-0">
+                            <small>{node.title}</small>
+                          </h6>
+                          <h6 className="mb-0 text-yellow">
+                            <small>${node.price}</small>
+                          </h6>
+                        </div>
+
+                        <p className="text-muted">
+                          <small>{node.description.description}</small>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
+            </div>
+          </div>
+        </section>
+      )
+    } else {
+      return (
+        <section className="menu py-5">
+          <div className="container">
+            <Title title="best of our menu" />
+            <div className="row">
+              <div className="col-10 col-sm-6 mx-auto text-center text-capitalize">
+                <h1>there are no items to display</h1>
+              </div>
+            </div>
+          </div>
+        </section>
+      )
+    }
+  }
+}
